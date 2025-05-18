e7effeed9b0917f1cb3c7b93a3a18ffe15f0ce03
Rosalie Alves <rosalieslv@gmail.com>
2025-05-17 22:04:21 -0300

    organizando Inimigos

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -2,13 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using TMPro;
 
 public class Score : MonoBehaviour
 {
     public int contador;
-    public Text score;
+    public TextMeshProUGUI score;
+    
+    public TextMeshProUGUI betterScore;
 
-    public void Pontuar() {
+    void Start()
+    {
+
+    }
+
+    public void Pontuar()
+    {
         contador += 1;
         score.text = "SCORE: " + contador.ToString();
     }
